6b759df34ad64170b421ac35a03c20d0fee04b2c
Aryan Goyal <goyal.aryan@gmail.com>
2025-07-20 23:28:38 +0530

    [FEAT] remove extra privilidges of admin
    Signed-off-by: Aryan Goyal <goyal.aryan@gmail.com>

--- middlewares/user_side.js
+++ middlewares/user_side.js
@@ -1,51 +1,51 @@
 import {get_food_item_name} from "../database_queries/database.js";
 export const customer_home = async (req, res, next) => {
-  if (req.user.role == "customer" || req.user.role == "admin") {
+  if (req.user.role == "customer") {
     next();
   } else {
-    const error = "You are not authorised to enter as this is a protected route and your are logged in as chef";
+    const error = "You are not authorised to enter as this is a protected route and your are logged in as chef or admin";
     res.render("error_page.ejs", { error })
   }
 };
 
 export const customer_menu = async (req, res, next) => {
-  if (req.user.role == "customer" || req.user.role == "admin") {
+  if (req.user.role == "customer") {
     next();
   } else {
     const error =
-      "You are not authorised to enter as this is a protected route and your are logged in as chef";
+      "You are not authorised to enter as this is a protected route and your are logged in as chef or admin";
     res.render("error_page.ejs", { error });
   }
 };
 
 export const chef_home = async (req, res, next) => {
-  if (req.user.role == "chef" || req.user.role == "admin") {
+  if (req.user.role == "chef") {
     next();
   } else {
     const error =
-      "You are not authorised to enter as this is a protected route and your are logged in as customer";
+      "You are not authorised to enter as this is a protected route and your are logged in as customer or admin";
     res.render("error_page.ejs", { error });
     
   }
 };
 
 export const chef_order = async (req, res, next) => {
-  if (req.user.role == "chef" || req.user.role == "admin") {
+  if (req.user.role == "chef") {
     next();
   } else {
     const error =
-      "You are not authorised to enter as this is a protected route and your are logged in as customer";
+      "You are not authorised to enter as this is a protected route and your are logged in as customer or admin";
     res.render("error_page.ejs", { error });
     
   }
 };
 
 export const chef_complete_item = async (req, res, next) => {
-  if (req.user.role == "chef" || req.user.role == "admin") {
+  if (req.user.role == "chef" ) {
     next();
   } else {
     const error =
-      "You are not authorised to enter as this is a protected route and your are logged in as customer";
+      "You are not authorised to enter as this is a protected route and your are logged in as customer or admin";
     res.render("error_page.ejs", { error });
     
   }
